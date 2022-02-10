9f8da492c23bc0af0813e061d6b7b591affe9e32
Ghostik-gh <54578428+Ghostik-gh@users.noreply.github.com>
2022-02-10 20:34:14 +0300

    fetch 1

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -1,12 +1,12 @@
-const token = '6c857c7502850f1fd06bd4de84dfdf4bb1a90b77795104e1cc2d29fcff3196dee35247277eefbb6770b9b';
+const token = 'f1633e94c4afd3f856f7766c45aa8557132b132ada93c4f189d33e77af3733053979925efe735d902029b';
 // https://oauth.vk.com/authorize?client_id=8074443&display=page&redirect_uri=https://ghostik-gh.github.io&scope=friends&response_type=token&v=5.131&state=123456
 // https://api.vk.com/method/friends.getOnline?v=5.131&access_token=dc678159de047caa96002fae5f76f548c75448c7313946b5b743fff06c830c9149f952741c6d828cafe67
 const client_id = 8074443;
 
 async function getResponse(){
-    let url = new URL('https://api.vk.com/method/users.get?v=5.131&access_token=6c857c7502850f1fd06bd4de84dfdf4bb1a90b77795104e1cc2d29fcff3196dee35247277eefbb6770b9b');
+    let url = new URL('https://api.vk.com/method/users.get?v=5.131&access_token=f1633e94c4afd3f856f7766c45aa8557132b132ada93c4f189d33e77af3733053979925efe735d902029b');
 
-    let response = fetch(url, {mode: "no-cors"});
+    let response = await fetch(url, {mode: "no-cors"});
     // console.log(response);
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
@@ -20,7 +20,7 @@ async function getResponse(){
 
 
 function getResponseF(){
-    let url = new URL('https://api.vk.com/method/users.get?v=5.131&access_token=6c857c7502850f1fd06bd4de84dfdf4bb1a90b77795104e1cc2d29fcff3196dee35247277eefbb6770b9b');
+    let url = new URL('https://api.vk.com/method/users.get?v=5.131&access_token=f1633e94c4afd3f856f7766c45aa8557132b132ada93c4f189d33e77af3733053979925efe735d902029b');
 
     let response = fetch(url, {mode: "no-cors"});
     console.log(response);
@@ -29,7 +29,7 @@ function getResponseF(){
         let object = response.json().then(console.log);
         console.log(object);
     } else {
-        alert("HTTP-Error: " + response.status);
+        console.log("HTTP-Error: " + response.status);
     }
     let object = response.json().then(console.log);
     console.log(object);
@@ -58,6 +58,6 @@ console.log(user_token)
 // console.log(params);
 // console.log('token --> ', params.get('access_token'));
 
-// fetch('https://api.vk.com/method/users.get?v=5.131&access_token=6c857c7502850f1fd06bd4de84dfdf4bb1a90b77795104e1cc2d29fcff3196dee35247277eefbb6770b9b', {mode: "no-cors"}).then(res => res.json()).then(console.log);
+// fetch('https://api.vk.com/method/users.get?v=5.131&access_token=f1633e94c4afd3f856f7766c45aa8557132b132ada93c4f189d33e77af3733053979925efe735d902029b', {mode: "no-cors"}).then(res => res.json()).then(console.log);
 
 // fetch(url, {mode: "no-cors"}) .then(res => res.json()) .then(console.log);
