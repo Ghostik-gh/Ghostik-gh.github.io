23905964524469b596f7deefc004216778bb9f04
Ghostik-gh <54578428+Ghostik-gh@users.noreply.github.com>
2022-02-19 17:48:28 +0300

    footer для всех страниц

---
+++ static/js/add_footer.js
@@ -0,0 +1,8 @@
+const footer = 
+`<footer class="card-footer">
+<div class="container">
+  <span class="text-muted">released by @ghostik-gh</span>
+</div>
+</footer>`;
+const body = document.body;
+body.insertAdjacentHTML("beforeend", footer);
